5c10e70c8aa78a82ef6412861c47d90218d517de
joaovictordevx <joaovictorfariasdev@gmail.com>
2024-09-25 08:56:21 -0300

    feat: :sparkles: Function LoginUser() estruturada

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -1,7 +1,20 @@
 <?php
     class AuthController {
         public function LoginUser() {
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $useremail = $_POST['email'];
+                $password = $_POST['password'];
 
+                $user = User::findByEmail($useremail);
+
+                if ($user && password_verify($password, $user['password'])) {
+                    session_start();
+
+                    $_SESSION['usuario_id'] = $user['id'];
+                    $_SESSION['perfil'] = $user['perfil'];
+                    header('Pow');
+                }
+            }
         }
     }
 ?>
