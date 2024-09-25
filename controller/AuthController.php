5479e5fce7076b62bee14434ba1c0f28e7c799e7
joaovictordevx <joaovictorfariasdev@gmail.com>
2024-09-25 10:16:13 -0300

    feat: :sparkles: Sistema de Rotas linkado e ajustados

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -1,19 +1,23 @@
 <?php
     class AuthController {
-        public function LoginUser() {
+        public function loginUser() {
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $useremail = $_POST['email'];
                 $password = $_POST['password'];
 
-                $user = User::findByEmail($useremail);
+                $user = UserModel::findByEmail($useremail);
 
                 if ($user && password_verify($password, $user['password'])) {
                     session_start();
 
                     $_SESSION['usuario_id'] = $user['id'];
                     $_SESSION['perfil'] = $user['perfil'];
-                    header('Pow');
+                    header('Location: index.php?action=dashborad');
+                } else {
+                    echo 'Invalid email or password';
                 }
+            } else {
+                include 'view/LoginView.php';
             }
         }
     }
