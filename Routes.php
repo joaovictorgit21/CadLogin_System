b0fdc50f156f6b6916c2af21fc3dd9a1c1617c8e
joaovictordevx <joaovictorfariasdev@gmail.com>
2024-09-25 10:14:16 -0300

    fix: :bug: Rotas ajustadas

--- Routes.php
+++ Routes.php
@@ -1,16 +1,23 @@
 <?php
     require 'controller/AuthController.php';
     require 'controller/UserController.php';
-    require 'controller/DashBoardController.php';
+    // require 'controller/DashBoardController.php';
 
-    $AuthController = new $AuthController();
-    $UserController = new $UserController();
-    $DashBoardController = new $DashBoardController();
+    $AuthController = new AuthController();
+    $UserController = new UserController();
+    // $DashBoardController = new DashBoardController();
 
-    $action = $_GEt['action'] ?? 'loginUser';
+    $action = $_GET['action'] ?? 'loginUser';
 
     switch($action) {
-        case 'loginUser':
-            $AuthController->LoginUser();
+        case 'LoginView':
+            $AuthController->loginUser();
+            break;
+        case 'RegisterView':
+            $UserController->registerUser();
+            break;
+        default:
+            $AuthController->loginUser();
+            break;
     }
 ?>
