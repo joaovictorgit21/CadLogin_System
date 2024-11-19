b1a9fef4d257ae5ae2bfaaa85df79c59abf46d34
joaovictorgit21 <joaovictorgit21@gmail.com>
2024-11-19 08:56:27 -0300

    fix: :bug: Autenticação de usuário corrigida e aprimorada

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -1,37 +1,44 @@
 <?php
 
 require_once 'model/UserModel.php';
-    class AuthController {
-        public function loginUser() {
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                $email = $_POST['email'];
-                $password = $_POST['password'];
 
-                $user = UserModel::findByEmail($email);
+class AuthController {
+    public function loginUser() {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
 
-                if (password_verify($password, $user['senha'])) {
-                    session_start();
-                    $_SESSION['id'] = $user['id'];
-                    $_SESSION['perfil'] = $user['perfil'];
+            // Busca o usuário pelo e-mail
+            $user = UserModel::findByEmail($email);
 
-                    header('Location: index.php?action=dashboard');
-                } else {
-                    include 'view/LoginView.php';
-                }
+            // Verifica se o usuário foi encontrado
+            if ($user && is_array($user) && password_verify($password, $user['senha'])) {
+                session_start();
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['perfil'] = $user['perfil'];
+
+                // Redireciona para o dashboard
+                header('Location: index.php?action=dashboard');
+                exit();
             } else {
+                // Usuário não encontrado ou senha incorreta
+                $error = "E-mail ou senha inválidos.";
                 include 'view/LoginView.php';
             }
-        }
-
-        public function logoutUser() {
-            // inicia a sessão do usuário para destruir
-            session_start();
-
-            // Destroi todas as infos da sessão
-            session_destroy();
-
-            // Redireciona o usuário para a página inicial
-            header('Location: index.php');  
+        } else {
+            include 'view/LoginView.php';
         }
     }
-?>
+
+    public function logoutUser() {
+        // Inicia a sessão do usuário para destruir
+        session_start();
+
+        // Destroi todas as informações da sessão
+        session_destroy();
+
+        // Redireciona o usuário para a página inicial
+        header('Location: index.php');  
+        exit();
+    }
+}
